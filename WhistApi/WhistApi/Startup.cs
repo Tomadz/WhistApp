4e1bda0c76b9d87c7311810dc117faafc49dd13d
Tomas R J <25370325+Tomadz@users.noreply.github.com>
2019-05-11 00:25:21 +0200

    Ændre navn på databasen til 'SpilMsDb'

--- WhistApi/WhistApi/Startup.cs
+++ WhistApi/WhistApi/Startup.cs
@@ -27,7 +27,7 @@ namespace WhistApi
                     if (resolver != null)
                         (resolver as DefaultContractResolver).NamingStrategy = null;
                 });
-            services.AddDbContext<RundeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("WhistDbConString")));
+            services.AddDbContext<RundeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SpilMsDbConString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
