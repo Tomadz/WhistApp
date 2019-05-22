aa5d1692ea0e579fb9ca571dd43f3a1b9126a3c3
Abergster <47314911+Abergster@users.noreply.github.com>
2019-05-22 11:39:07 +0200

    update mega microservice

--- SpilService/SpilService/SqlQuery.cs
+++ SpilService/SpilService/SqlQuery.cs
@@ -127,5 +127,7 @@ namespace SpilService
             return plusser;
 
         }
+
+
     }
 }
--- SpilService/SpilService/Startup.cs
+++ SpilService/SpilService/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -26,6 +27,8 @@ namespace SpilService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<Subscribe>();
+            services.AddSingleton<IHostedService, Subscribe>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
