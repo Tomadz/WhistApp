f0bf4609e7cba90130c117b7f6a80c4aa2bee735
Abergster <47314911+Abergster@users.noreply.github.com>
2019-05-22 11:37:58 +0200

    update mega microservice

---
+++ SpilService/SpilService/SqlQueryInsert.cs
@@ -0,0 +1,43 @@
+﻿using SpilService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpilService
+{
+    public class SqlQueryInsert
+    {
+        private SqlConnection conn = null;
+        public SqlQueryInsert()
+        {
+            conn = DBConn.GetConn();
+        }
+
+        public void RunderInsert(Runde item)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd = new SqlCommand("INSERT INTO Runder " +
+                "Values (" + item.SpilId + "," + item.RundeNr + "," + item.Melder + "," + item.Melding + "," +
+                item.PlusId + "," + item.Makker + "," + item.Vundet + "," + item.Beløb + "," +
+                item.Spiller1 + "," + item.Spiller2 + "," + item.Spiller3 + "," + item.Spiller4 + ")", conn);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+        public void RunderDelete(int id)
+        {/*
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd = new SqlCommand("delete Runder where id", conn);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();*/
+        }
+
+    }
+}
---
+++ SpilService/SpilService/Subscribe.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using SpilService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpilService
+{
+    public class Subscribe : BackgroundService
+    {
+
+
+        public Subscribe()
+        {
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                SqlQueryInsert queryInsert = new SqlQueryInsert();   
+                SubscribeDelete(channel,queryInsert);
+                SubscribeRunde(channel,queryInsert);
+                await Task.Delay(1);
+                while (true) { }
+            }
+        }
+
+        void SubscribeRunde(IModel channel, SqlQueryInsert queryInsert)
+        {
+            channel.ExchangeDeclare(exchange: "Runde", type: "fanout");
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "Runde",
+                              routingKey: "");
+
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+
+                Runde item = Newtonsoft.Json.JsonConvert.DeserializeObject<Runde>(message);
+                queryInsert.RunderInsert(item);    
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            /*
+            channel.QueueDeclare(queue: "Opret",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);*/
+            /*
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "Opret",
+                                 autoAck: true,
+                                 consumer: consumer);*/
+
+
+
+        }
+        void SubscribeDelete(IModel channel, SqlQueryInsert queryInsert)
+        {
+            channel.ExchangeDeclare(exchange: "Delete", type: "fanout");
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "Delete",
+                              routingKey: "");
+
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var id = Encoding.UTF8.GetString(body);
+
+
+            };
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            /*
+            channel.QueueDeclare(queue: "Opret",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);*/
+            /*
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "Opret",
+                                 autoAck: true,
+                                 consumer: consumer);*/
+
+        }
+        }
